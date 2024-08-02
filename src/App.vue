892d8dc6023cc752367dcb9ee3ec0dc5cb65ed9c
boomiiing <1215574240@qq.com>
2024-08-02 16:57:13 +0800

    提交未格式化代码!

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,17 @@
   </div>
 </template>
 <script setup lang="ts">
-var str = 'ads'
-for (let i = 0; i < 3; i++) {
+var str = 'ads';
+for (let i = 0; i < 3; i++) 
+{
+ 
   console.log(i)
-}
+};
 </script>
 <style scoped lang="scss">
 .box {
   width: 100px;
-
   height: 100px;
-
   background-color: plum;
 }
 </style>
