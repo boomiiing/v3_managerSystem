f9d535c69b340943be8d2079efa1a8394f976931
boomiiing <1215574240@qq.com>
2024-08-02 17:31:44 +0800

    引入element plus

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,11 @@
 <template>
   <div>
-    <div class="box">我是apasdp</div>
+    <el-button type="success">按钮</el-button>
   </div>
 </template>
 <script setup lang="ts">
-var str = 'ads';
-for (let i = 0; i < 3; i++) 
-{
- 
-  console.log(i)
-};
+
 </script>
 <style scoped lang="scss">
-.box {
-  width: 100px;
-  height: 100px;
-  background-color: plum;
-}
+
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
 import './style.css'
-import App from './App.vue'
+import App from '@/App.vue'
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css'
+// @ts-ignore
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+const app = createApp(App)
+app.use(ElementPlus, {
+  locale: zhCn
+})
+app.mount('#app')
 
-createApp(App).mount('#app')
-const fun = () => {
-  console.log('asd')
-}
-
-fun()
